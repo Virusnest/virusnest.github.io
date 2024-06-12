2620b682449a645e9b26c5d6e97e366698710419
Virusnest <72645466+Virusnest@users.noreply.github.com>
2024-06-12 16:02:53 +1000

    oopsi poopsi

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ async function fillTheTitle() {
   const post = await fetch("https://api.github.com/users/virusnest/followers").then((res) => res.json());
   document.getElementById("folowers").innerText = post.length;
   const posts = await fetch("https://api.github.com/users/virusnest/following").then((res) => res.json());
-  document.getElementById("folowing").innerText = post.length;
+  document.getElementById("folowing").innerText = posts.length;
 }
 fillTheTitle();
